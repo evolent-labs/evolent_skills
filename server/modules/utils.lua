d19047cc07178e443fc230f92ab74d73d7c9c691
Roberts Bauers <rrbauerss@gmail.com>
2024-06-02 23:58:44 +0300

    fix(utils): properly calculate level ranges

--- server/modules/utils.lua
+++ server/modules/utils.lua
@@ -2,8 +2,6 @@ local conf = require 'config'
 
 local utils = {}
 
----@param skillName string
----@return table
 local function precomputeXpRequirements(skillName)
     local skillConfig = conf.Skills[skillName]
     if not skillConfig then
@@ -11,58 +9,45 @@ local function precomputeXpRequirements(skillName)
     end
 
     local xpTable = {}
-    local baseXp = skillConfig.baseXp
-    local nextLevelMultiplier = skillConfig.nextLevelMultiplier
-    local maxLevel = skillConfig.maxLevel
-    local requiredXp = baseXp
+    local currentMaxXp = skillConfig.baseXp
+    xpTable[1] = currentMaxXp
 
-    for level = 1, maxLevel do
-        xpTable[level] = requiredXp
-        requiredXp = requiredXp * nextLevelMultiplier
+    for level = 2, skillConfig.maxLevel do
+        currentMaxXp = math.ceil(currentMaxXp * skillConfig.nextLevelMultiplier)
+        xpTable[level] = currentMaxXp
     end
 
     return xpTable
 end
 
 utils.xpTables = {}
-for skillName, _ in pairs(conf.Skills) do
+for skillName in pairs(conf.Skills) do
     utils.xpTables[skillName] = precomputeXpRequirements(skillName)
 end
 
----@param xpAmount number
----@param skillName string
 function utils.calculateLevel(xpAmount, skillName)
-    local skillConfig = conf.Skills[skillName]
-    if not skillConfig then
-        error(('Skill \'%s\' is not configured!'):format(skillName), 1)
-    end
-
-    local level = 1
-    local maxLevel = skillConfig.maxLevel
     local xpTable = utils.xpTables[skillName]
-
-    while xpAmount >= xpTable[level] and level < maxLevel do
-        xpAmount = xpAmount - xpTable[level]
+    local level = 1
+    while level < #xpTable and xpAmount >= xpTable[level] do
         level = level + 1
     end
-
     return level
 end
 
----@param level number
----@param skillName string
----@return number minXp, number maxXp
 function utils.getXpRangeForLevel(level, skillName)
     local xpTable = utils.xpTables[skillName]
     if not xpTable then
         error(('Skill \'%s\' is not configured!'):format(skillName), 1)
     end
 
-    local minXp = 0
-    for i = 1, level - 1 do
-        minXp = minXp + xpTable[i]
+    local minXp = (level > 1) and xpTable[level - 1] or 0
+    local maxXp
+    if level < #xpTable then
+        maxXp = xpTable[level]
+    else
+        maxXp = math.ceil(xpTable[level] * conf.Skills[skillName].nextLevelMultiplier)
     end
-    local maxXp = minXp + xpTable[level]
+
     return minXp, maxXp
 end
 
